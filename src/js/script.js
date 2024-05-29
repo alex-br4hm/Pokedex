011e9b80132e4a4c9e8cf2fde6812e17cc45de1c
br4hm <haehnlein.alexander@gmx.de>
2024-05-29 16:56:25 +0200

    add: pokemonId to card

--- src/js/script.js
+++ src/js/script.js
@@ -21,22 +21,35 @@ async function getAllData() {
       console.log(data);
       if (data) {
          let pokeName = capitalizeFirstLetter(data.forms[0].name);
+         let pokeId = data.id; // Keep pokeId as a number initially
          let pokePic = data.sprites.other.dream_world.front_default;
-         // let pokePic = data.sprites.versions.generation-i.red-blue.front_transparent;
          let pokeType = data.types[0].type.name;
-         // if (data.types[1].type.name != undefined) {
-         //    let pokeType2 = data.types[1].type.name;
-         //    return pokeType2;
-         // }
+         let pokeType2 = data.types[1] ? data.types[1].type.name : null;
 
-         // console.log(pokeType2);
+         // Determine the format of the ID without converting to a string
+         let formattedId;
+         if (pokeId < 10) {
+            formattedId = `#00${pokeId}`;
+         } else if (pokeId < 100) {
+            formattedId = `#0${pokeId}`;
+         } else {
+            formattedId = `#${pokeId}`;
+         }
+
+         // Display the Pokémon card with one or two types
          contentContainer.innerHTML += `
-         <div class="pokemon-card">
-         <div class="poke-name">${pokeName}</div>
-         <div class="poke-pic-wrapper ${pokeType}">
-         <img class="poke-pic"src="${pokePic}" alt="">
-         </div>
-         <div>${pokeType} / ${pokeType}</div>
+         <div class="pokemon-card ${pokeType}">
+            <div class="card-name-id-wrapper">
+            <div class="poke-name">${pokeName}</div>
+            <div class="poke-id">${formattedId}</div>
+            </div>
+            <div class="poke-pic-wrapper">
+               <img class="poke-pic" src="${pokePic}" alt="${pokeName}">
+            </div>
+            <div class="poke-type-wrapper">
+               <div class="${pokeType} poke-type">${pokeType}</div>
+               ${pokeType2 ? `<div class="${pokeType2} poke-type">${pokeType2}</div>` : ""}
+            </div>
          </div>
          `;
       } else {
@@ -44,7 +57,6 @@ async function getAllData() {
       }
    }
 }
-
 function capitalizeFirstLetter(string) {
    return string.charAt(0).toUpperCase() + string.slice(1);
 }
