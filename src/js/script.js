5b3e854358f1f17a6f4c82676fb9bc0350f96814
br4hm <haehnlein.alexander@gmx.de>
2024-05-29 23:50:35 +0200

    style: popup-card

--- src/js/script.js
+++ src/js/script.js
@@ -29,7 +29,7 @@ async function getData(pokeOrder) {
 function getAllDatasDummy() {}
 
 async function getAllData() {
-   for (let i = 1; i <= 10; i++) {
+   for (let i = 1; i <= 50; i++) {
       const data = await getData(i);
       console.log(data);
       if (data) {
@@ -75,6 +75,7 @@ function getPokeInformations(data) {
 
 async function getMoreInformation(i) {
    pokeText = await getFirstFlavorText(i);
+   pokeText = pokeText.replace(/[\n\f]/g, " ");
    return;
 }
 
@@ -95,10 +96,14 @@ function capitalizeFirstLetter(string) {
 
 function openPopUp(i) {
    popUpContainer.classList.remove("d-none");
+   document.body.classList.add("unscrollable");
+   contentContainer.classList.add("blured");
    renderPopUpContainer(i);
 }
 
 function closePopUp() {
+   document.body.classList.remove("unscrollable");
+   contentContainer.classList.remove("blured");
    popUpContainer.classList.add("d-none");
 }
 
@@ -109,16 +114,25 @@ async function renderPopUpContainer(i) {
       getPokeInformations(data, i);
       await getMoreInformation(i);
       popUpContainer.innerHTML += `
-       <div class="poke-pu-wrapper">
+       <div class="poke-pu-wrapper ${pokeType}PU">
             <h2>${pokeName}</h2>
             <img class="close-icon" src="./src/assets/img/close_icon.png" onclick="closePopUp()" alt="" />
+            <img class="arrow-icon arrow-left" src="./src/assets/img/arrow_icon.png" onclick="" alt="" />
+            <img class="arrow-icon arrow-right" src="./src/assets/img/arrow_icon.png" onclick="" alt="" />
+            <img src="${pokePic}" class="poke-pu-pic" alt="Picture of ${pokeName}" />
+            <div class="poke-pu-information-wrapper">
+            <div class="poke-type-wrapper">
+            <div class="${pokeType}PU poke-type">${pokeType}</div>
+            ${pokeType2 ? `<div class="${pokeType2}PU poke-type">${pokeType2}</div>` : ""}
+         </div>
+            <div class="flavor-text">
+            ${pokeText}
+            </div>
             <div class="stats">HP: ${statHP}</div>
             <div class="stats">Attack: ${statAttack}</div>
             <div class="stats">Defense: ${statDefense}</div>
             <div class="stats">Speed: ${statSpeed}</div>
-            <img src="${pokePic}" class=" ${pokeType}" alt="" />
-            <div class="flavor-text">
-            ${pokeText}
+            
             </div>
          </div>
       `;
@@ -138,7 +152,7 @@ async function getFirstFlavorText(pokemonOrder) {
       const flavorTextEntries = data.flavor_text_entries;
       if (flavorTextEntries && flavorTextEntries.length > 0) {
          const firstFlavorText = flavorTextEntries[0].flavor_text;
-         // console.log(firstFlavorText);
+         console.log(firstFlavorText);
          return firstFlavorText;
       } else {
          console.log("No flavor text entries found.");
